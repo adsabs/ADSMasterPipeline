388960eacd85fbad54d323a11785ffd67163df5e
tjacovich <tjacovich@cfa.harvard.edu>
2023-11-20 15:33:29 -0500

    Additional tweaks to make sure downgrade and upgrade can always be envoked.

--- alembic/versions/2d2af8a9c996_upgrade_change_log.py
+++ alembic/versions/2d2af8a9c996_upgrade_change_log.py
@@ -18,7 +18,7 @@ import sqlalchemy as sa
 
 def upgrade():
     op.execute('ALTER TABLE change_log ADD COLUMN big_id BIGINT;')
-    op.execute('CREATE FUNCTION set_new_id() RETURNS TRIGGER AS\n'\
+    op.execute('CREATE OR REPLACE FUNCTION set_new_id() RETURNS TRIGGER AS\n'\
                    '$BODY$\n'\
                    'BEGIN\n'\
                    '\t NEW.big_id := NEW.id;\n'\
@@ -43,14 +43,16 @@ def upgrade():
 
 
 def downgrade():
-    op.add_column('change_log', sa.Column('smallid', sa.Integer(), unique=True))
+    op.add_column('change_log', sa.Column('small_id', sa.Integer(), unique=True))
     op.execute('DELETE FROM change_log WHERE id > 2147483647')
-    op.execute('UPDATE change_log SET smallid=id')
-    op.alter_column('change_log', 'smallid', nullable=False)
+    op.execute('UPDATE change_log SET small_id=id')
+    op.alter_column('change_log', 'small_id', nullable=False)
     op.drop_constraint('change_log_pkey', 'change_log', type_='primary')
-    op.create_primary_key("change_log_pkey", "change_log", ["smallid", ])
+    op.create_primary_key("change_log_pkey", "change_log", ["small_id", ])
+    op.execute('ALTER SEQUENCE change_log_id_seq OWNED BY change_log.small_id;')
+    op.execute("ALTER TABLE change_log ALTER COLUMN small_id SET DEFAULT nextval('change_log_id_seq');")
     op.alter_column('change_log', 'id', nullable=False, new_column_name='old_id')
-    op.alter_column('change_log', 'smallid', nullable=False, new_column_name='id')
+    op.alter_column('change_log', 'small_id', nullable=False, new_column_name='id')
     op.drop_column('change_log', 'old_id')
-    op.execute('ALTER SEQUENCE change_log_id_seq as integer MAXVALUE 2147483647')
+    op.execute('ALTER SEQUENCE change_log_id_seq as int MAXVALUE 2147483647')
 
