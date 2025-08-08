821f9a6e48b42a14f23ee8a6c9743b20eb52f72b
tjacovich <tjacovich@cfa.harvard.edu>
2025-08-08 09:36:13 -0400

    Fix SciX ID test.

--- adsmp/tests/test_solr_updater.py
+++ adsmp/tests/test_solr_updater.py
@@ -340,7 +340,7 @@ class TestSolrUpdater(unittest.TestCase):
                 "volume",
             ],
         )
-        self.assertEqual(x["scix_id"], "scix:0R7S-E4PT-52FF")
+        self.assertEqual(x["scix_id"], "scix:42MM-89VE-90A0")
         self.assertEqual(round(x["doctype_boost"],3),0.857)
 
         self.app.update_storage(
