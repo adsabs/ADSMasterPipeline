1d437058fec341fbad66703bc3eb103a1e098a65
femalves <fernandamalves@ufrn.edu.br>
2025-08-14 13:29:53 -0400

    Removing cache_ok

--- adsmp/models.py
+++ adsmp/models.py
@@ -20,7 +20,6 @@ MetricsBase = declarative_base()
 
 class UTCDateTime(types.TypeDecorator):
     impl = TIMESTAMP
-    cache_ok = False
 
     def process_bind_param(self, value, engine):
         if isinstance(value, basestring):
