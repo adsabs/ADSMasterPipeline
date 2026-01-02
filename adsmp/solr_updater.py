3aabfc543dd740b3143b28864d2027e391c661a3
thomasallen <tom.sco@gmail.com>
2026-01-02 13:20:49 -0800

    Fix extract_classifications_pipeline empty string bug

--- adsmp/solr_updater.py
+++ adsmp/solr_updater.py
@@ -179,6 +179,7 @@ def extract_classifications_pipeline(db_classifications, solrdoc):
     """retrieve expected classifier collections
 
     classifications is a solr virtual field so it should never be set"""
+    db_classifications = [element for element in db_classifications if element] # remove empty strings
     if db_classifications is None or len(db_classifications) == 0:
         return {"database" : solrdoc.get("database", None)}
 
